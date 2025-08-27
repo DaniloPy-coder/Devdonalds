8bfdebb545ce35b4d9327983bc447050fa625d87
Danilo <danilodevsp@gmail.com>
2025-08-27 15:19:00 -0300

    chore: adicionei dois plugins: um de ordenar eslint e outro para o tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
+
 const HomePage = () => {
   return (
-    <div className="p-5 border border-red-500 rounded-xl">
-      <h1 className="text-red-500">Welcome to Dev Donalds</h1>
-    </div>
+    <h1 className="text-red-500">Welcome to Dev Donalds</h1>
   );
 }
 
