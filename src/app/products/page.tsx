e736488cc0dc6b4af0d897ce830db365c2c3b609
Danilo <danilodevsp@gmail.com>
2025-08-27 15:03:01 -0300

    chore: atualizei as fontes"

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 
 const ProductsPage = () => {
     return (
-        <div>
+        <div className="p-5 border border-red-500 rounded-xl">
             <h1>Products Page</h1>
             <Button>Add to Cart</Button>
             <Input placeholder="Search products..." />
