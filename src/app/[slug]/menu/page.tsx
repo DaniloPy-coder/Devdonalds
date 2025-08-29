49b00571447f493d0c0afbb805e4d12e205649bc
Danilo <danilodevsp@gmail.com>
2025-08-29 14:31:16 -0300

    feat: add initial restaurant page and menu page

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,47 @@
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import RestaurantHeader from "./components/header";
+
+interface RestaurantsMenuPageProps {
+    params: Promise<{
+        slug: string;
+    }>;
+    searchParams: Promise<{
+        consumptionMethod?: string;
+    }>;
+}
+
+
+// apenas uma definição da função
+const isConsumptionMethodValid = (consumptionMethod?: string) => {
+    if (!consumptionMethod) return false;
+    return ["DINE_IN", "TAKE_AWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantsMenuPage = async ({ params, searchParams }: RestaurantsMenuPageProps) => {
+    const { slug } = await params;
+    const { consumptionMethod } = await searchParams;
+
+    // valida antes de buscar o restaurante
+    if (!isConsumptionMethodValid(consumptionMethod)) {
+        return notFound();
+    }
+
+    const restaurant = await db.restaurant.findUnique({
+        where: { slug },
+    });
+
+    if (!restaurant) {
+        return notFound();
+    }
+
+    return (
+        <div>
+            <RestaurantHeader restaurant={restaurant} />
+        </div>
+    );
+};
+
+export default RestaurantsMenuPage;
---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image"
+import { notFound } from "next/navigation"
+
+import { db } from "@/lib/prisma"
+
+import ConsuptionMethodOption from "./components/consuption-method-option"
+
+interface RestaurantPageprops {
+    params: Promise<{ slug: string }>
+}
+
+const RestaurantPage = async ({ params }: RestaurantPageprops) => {
+    const { slug } = await params
+    const restaurant = await db.restaurant.findUnique({ where: { slug } })
+    if (!restaurant)
+        return notFound()
+    return (
+        <div className="h-screen flex flex-col items-center justify-center px-6 pt-24">
+            <div className="flex flex-col items-center gap-2">
+                <Image src={restaurant?.avatarImageUrl} alt={restaurant?.name} width={82} height={82} />
+                <h2 className="font-semibold">{restaurant?.name}</h2>
+            </div>
+
+            <div className="pt-24 text-center space-y-2">
+                <h3 className="text-2xl font-semibold">Seja bem vindo!</h3>
+                <p className="opacity-55">Escolha como prefere aproveitar sua refeição</p>
+            </div>
+
+            <div className="pt-14 grid grid-cols-2 gap-4">
+                <ConsuptionMethodOption
+                    buttonText="Para comer aqui"
+                    imageAlt="Comer aqui"
+                    imageUrl="/Dine_in.png"
+                    option="DINE_IN"
+                    slug={slug}
+                />
+                <ConsuptionMethodOption
+                    buttonText="Para levar"
+                    imageAlt="levar"
+                    imageUrl="/takeaway.png"
+                    option="TAKE_AWAY"
+                    slug={slug}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default RestaurantPage;
