dad9d10f761c3387ec359c640b5a5e38551f2967
Danilo <danilodevsp@gmail.com>
2025-09-01 12:10:18 -0300

    feat: add product header component

---
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -0,0 +1,45 @@
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ProductHeader from "./components/product_header";
+
+interface ProductPageProps {
+    params: Promise<{
+        slug: string;
+        productId: string;
+    }>;
+}
+
+const ProductPage = async ({ params }: ProductPageProps) => {
+    const { slug, productId } = await params;
+    const product = await db.product.findUnique({
+        where: {
+            id: productId
+        },
+        include: {
+            restaurant: {
+                select: {
+                    name: true,
+                    avatarImageUrl: true,
+                    slug: true
+                }
+            }
+        }
+    });
+
+    if (!product) {
+        notFound();
+    }
+    if (product.restaurant.slug.toUpperCase() !== slug.toUpperCase()) {
+        return notFound();
+    }
+
+    return (
+        <div className="flex h-full flex-col">
+            <ProductHeader product={product} />
+        </div>
+    )
+};
+
+export default ProductPage;
--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,25 +1,27 @@
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
+import { useParams } from "next/navigation";
+
+import { formatCurrency } from "@/helpers/format_currency";
 
 interface ProductProps {
     products: Product[]
 }
 
 const Products = ({ products }: ProductProps) => {
+    const { slug } = useParams<{ slug: string }>()
+
     return (
         <div className="space-y-3 px-5">
             {products.map((product) => (
-                <Link key={product.id} href="/" className="flex border-b items-center justify-between gap-0 py-3">
+                <Link key={product.id} href={`/${slug}/menu/${product.id}`} className="flex border-b items-center justify-between gap-0 py-3">
                     {/* Esquerda */}
                     <div>
                         <h3 className="text-sm font-medium">{product.name}</h3>
                         <p className="line-clamp-2 text-sm text-muted-foreground">{product.description}</p>
                         <p className="pt-3 text-sm font-semibold">
-                            {new Intl.NumberFormat('pt-BR', {
-                                style: 'currency',
-                                currency: 'BRL'
-                            }).format(product.price)}
+                            {formatCurrency(product.price)}
                         </p>
                     </div>
 
