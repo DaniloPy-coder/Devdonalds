f3023036be0d8870616ddce11712096cadb77e82
Danilo <danilodevsp@gmail.com>
2025-09-02 18:03:36 -0300

    feat: adicionei o componente inicial do cart product item(sem funcionalidades)

--- src/app/[slug]/menu/components/cart_sheet.tsx
+++ src/app/[slug]/menu/components/cart_sheet.tsx
@@ -3,13 +3,13 @@ import { useContext } from "react";
 import {
     Sheet,
     SheetContent,
-    SheetDescription,
     SheetHeader,
     SheetTitle,
     SheetTrigger,
 } from "@/components/ui/sheet";
 
 import { CartContext } from "../contexts/cart";
+import CartProductItem from "./cart_product_item";
 
 const CartSheet = () => {
     const { isOpen, toggleCart, products } = useContext(CartContext);
@@ -17,19 +17,15 @@ const CartSheet = () => {
     return (
         <Sheet open={isOpen} onOpenChange={toggleCart}>
             <SheetTrigger>Open</SheetTrigger>
-            <SheetContent>
+            <SheetContent className="w-[80%]">
                 <SheetHeader>
-                    <SheetTitle>Are you absolutely sure?</SheetTitle>
-                    <SheetDescription>
-                        This action cannot be undone. This will permanently delete your
-                        account and remove your data from our servers.
-                    </SheetDescription>
+                    <SheetTitle className="text-left">Sacola</SheetTitle>
                 </SheetHeader>
-                {products.map((product) => (
-                    <h1 key={product.id}>
-                        {product.name} - {product.quantity}
-                    </h1>
-                ))}
+                <div className="py-5">
+                    {products.map((product) => (
+                        <CartProductItem key={product.id} product={product} />
+                    ))}
+                </div>
             </SheetContent>
         </Sheet>
     );
