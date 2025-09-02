f59c81d60c9b925f5e43a018770d8067d720395e
Danilo <danilodevsp@gmail.com>
2025-09-02 12:53:10 -0300

    feat: adicionei o contexto do carrinho e o componente sheet

--- src/app/[slug]/menu/[productId]/components/products_details.tsx
+++ src/app/[slug]/menu/[productId]/components/products_details.tsx
@@ -3,105 +3,121 @@
 import { Prisma } from "@prisma/client";
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { formatCurrency } from "@/helpers/format_currency";
 
+import CartSheet from "../../components/cart_sheet";
+import { CartContext } from "../../contexts/cart";
+
 interface ProductDetailProps {
-    product: Prisma.ProductGetPayload<{
-        include: {
-            restaurant: {
-                select: {
-                    name: true;
-                    avatarImageUrl: true;
-                };
-            };
+  product: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          name: true;
+          avatarImageUrl: true;
         };
-    }>;
+      };
+    };
+  }>;
 }
 
 const ProductDetails = ({ product }: ProductDetailProps) => {
-    const [quantity, setQuantity] = useState<number>(1);
-    const handleDecreaseQuantity = () => {
-        if (quantity > 1) {
-            setQuantity(quantity - 1);
-        }
-    };
-    const handleIncreaseQuantity = () => {
-        setQuantity(quantity + 1);
-    };
+  const { toggleCart } = useContext(CartContext);
+  const [quantity, setQuantity] = useState<number>(1);
+  const handleDecreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+  const handleIncreaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+  const handleAddToCart = () => {
+    toggleCart();
+  };
 
-    return (
-        <div className="relative z-50 flex-col overflow-hidden flex mt-[-1.5rem] flex-auto rounded-t-3xl px-5 py-5">
-            <div className="flex-auto overflow-hidden">
-                {/* Restaurante */}
-                <div className="flex items-center gap-1">
-                    <Image
-                        src={product.restaurant.avatarImageUrl}
-                        alt={product.restaurant.name}
-                        width={16}
-                        height={16}
-                        className="rounded-full"
-                    />
-                    <p className="text-xs text-muted-foreground">
-                        {product.restaurant.name}
-                    </p>
-                </div>
+  return (
+    <>
+      <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col overflow-hidden rounded-t-3xl px-5 py-5">
+        <div className="flex-auto overflow-hidden">
+          {/* Restaurante */}
+          <div className="flex items-center gap-1">
+            <Image
+              src={product.restaurant.avatarImageUrl}
+              alt={product.restaurant.name}
+              width={16}
+              height={16}
+              className="rounded-full"
+            />
+            <p className="text-xs text-muted-foreground">
+              {product.restaurant.name}
+            </p>
+          </div>
 
-                {/* Preço e Produto */}
-                <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
+          {/* Preço e Produto */}
+          <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
 
-                {/* Preço e Quantidade */}
-                <div className="flex items-center justify-between">
-                    <h3 className="text-xl font-semibold">
-                        {formatCurrency(product.price)}
-                    </h3>
-                    <div className="flex items-center gap-3 text-center">
-                        <Button
-                            variant="outline"
-                            onClick={handleDecreaseQuantity}
-                            className="h-8 w-8 rounded-xl"
-                        >
-                            <ChevronLeftIcon />
-                        </Button>
-                        <p className="w-4">{quantity}</p>
-                        <Button
-                            variant="destructive"
-                            onClick={handleIncreaseQuantity}
-                            className="h-8 w-8 rounded-xl"
-                        >
-                            <ChevronRightIcon />
-                        </Button>
-                    </div>
-                </div>
+          {/* Preço e Quantidade */}
+          <div className="flex items-center justify-between">
+            <h3 className="text-xl font-semibold">
+              {formatCurrency(product.price)}
+            </h3>
+            <div className="flex items-center gap-3 text-center">
+              <Button
+                variant="outline"
+                onClick={handleDecreaseQuantity}
+                className="h-8 w-8 rounded-xl"
+              >
+                <ChevronLeftIcon />
+              </Button>
+              <p className="w-4">{quantity}</p>
+              <Button
+                variant="destructive"
+                onClick={handleIncreaseQuantity}
+                className="h-8 w-8 rounded-xl"
+              >
+                <ChevronRightIcon />
+              </Button>
+            </div>
+          </div>
 
-                <ScrollArea className="h-full">
-                    {/* Sobre */}
-                    <div className="mt-6 space-y-3">
-                        <h4 className="font-semibold">Sobre</h4>
-                        <p className="text-sm text-muted-foreground">{product.description}</p>
-                    </div>
+          <ScrollArea className="h-full">
+            {/* Sobre */}
+            <div className="mt-6 space-y-3">
+              <h4 className="font-semibold">Sobre</h4>
+              <p className="text-sm text-muted-foreground">
+                {product.description}
+              </p>
+            </div>
 
-                    {/* Ingredientes */}
-                    <div className="mt-6 space-y-3">
-                        <div className="flex items-center gap-1.5">
-                            <ChefHatIcon size={18} />
-                            <h4 className="font-semibold">Ingredientes</h4>
-                        </div>
-                        <ul className="list-disc px-5 text-sm text-muted-foreground">
-                            {product.ingredients.map((ingredient) => (
-                                <li key={ingredient} className="flex items-center gap-1.5">{ingredient}</li>
-                            ))}
-                        </ul>
-                    </div>
-                </ScrollArea>
-            </div >
+            {/* Ingredientes */}
+            <div className="mt-6 space-y-3">
+              <div className="flex items-center gap-1.5">
+                <ChefHatIcon size={18} />
+                <h4 className="font-semibold">Ingredientes</h4>
+              </div>
+              <ul className="list-disc px-5 text-sm text-muted-foreground">
+                {product.ingredients.map((ingredient) => (
+                  <li key={ingredient} className="flex items-center gap-1.5">
+                    {ingredient}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </ScrollArea>
+        </div>
 
-            <Button className="w-full rounded-full">Adicionar à sacola</Button>
-        </div >
-    );
+        <Button onClick={handleAddToCart} className="w-full rounded-full">
+          Adicionar à sacola
+        </Button>
+      </div>
+      <CartSheet />
+    </>
+  );
 };
 
 export default ProductDetails;
